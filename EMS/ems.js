3a7d4502cd5d32e2007d1f856429133f52dd72b2
Merisa <bishwakarma.merisa@gmail.com>
2022-04-30 13:12:04 +0100

    task

--- EMS/ems.js
+++ EMS/ems.js
@@ -10,11 +10,12 @@ let records = [
         empno:1,
         empname: "John",
         dept: 1,
+        salary:2200,
     }
 ]
 
 app.get('/', function(req,res){
-            res.render("homepage.ejs")
+            res.render("homepage.ejs",{employee: records})
         })
 
 app.get('/entry', function(req,res){
@@ -22,9 +23,6 @@ app.get('/entry', function(req,res){
 
 })
 
-app.get('/showdata', function(req,res){
-    res.render("showdata.ejs", {employee: records})
-})
 
 app.post('/processform', function(req,res){
 
@@ -33,23 +31,34 @@ app.post('/processform', function(req,res){
         empname: req.body.empname,
         dept: req.body.dept
     })
-    res.render("showdata.ejs", {employee: records})
+    res.render("homepage.ejs", {employee: records})
 
 })
 
 app.get('/deleterecord/:rec', function(req,res){
     let x = parseInt(req.params.rec)
     records.splice(x,1)
-    res.render('showdata.ejs', {employee: records})
+    res.render('homepage.ejs', {employee: records})
 })
 
 app.get('/updaterecord/:rec', function(req,res){
     res.render('updaterecord.ejs')
 })
 
-app.get('/employeedetail/:T', function(req,res){
-    let empdetail = req.params.T
-    res.render('employeedetail.ejs', {employeename: records}) 
+app.get('/employeedetail', function(req,res){
+    let emp = ({
+        empno: req.body.empno,
+        empname: req.body.empname,
+        dept: req.body.dept,
+        salary: req.body.salary
+    })
+ 
+    res.render('employeedetail.ejs', {employee: emp}) 
 
+})  
+
+app.post('/:T', function(req,res){
+    console.log(req.params)
+    let T = req.params.T
 })
 app.listen(4005)
